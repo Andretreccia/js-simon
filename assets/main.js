0b2c54666236d41dc0c762893f97fa3cd05b8da4
Andretreccia <andreabtreccia@gmail.com>
2021-10-27 15:27:18 +0200

    Creata funzione per generare numeri casuali

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,26 @@ Da li parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
+//creare un array di numeri
+const numbersArray = []
+//creare una funzione che aggiunge cinque numeri casuali nell array
+
+numberPushAndRandom(numbersArray)
+console.log(numbersArray)
+//selezionare il bottnne nella DOM
+//al click i numeri vengono visualizzati con un alert
+//dopo 30 secondi
+//innerHTML vuoto per cancellare i numeri
+//5 prompt per inserire i numeri
+//condizioni per confrontare i numeri inseriti
+//inserire nella DOM quali numeri sono uguali e quali no
+
+
+//funzioni
+function numberPushAndRandom(array) {
+    while (array.length < 5) {
+        let number = Math.floor(Math.random() * 100);
+        array.push(number)
+    }
+
+}
